245ac833f109b3b9a1aa394bdb0fcaa20825f4ad
Nisarg Mehta <nm555@cam.ac.uk>
2017-06-13 10:50:24 +0100

    Display parking start time

--- app/src/main/java/com/kascayd/prototype/MainActivity.java
+++ app/src/main/java/com/kascayd/prototype/MainActivity.java
@@ -4,16 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import java.util.Calendar;
+//import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
+    //private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     public void StartPress(View view) {
+        String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+        String message = "You have parked at _____. \n Start : "+ mydate;
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    public void StopPress(View view){
         String message = "You have parked at _____";
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(message);
