37dfa101ce691c9343b2ad0d45747dc1f7bd4ead
Nisarg Mehta <nm555@cam.ac.uk>
2017-06-13 10:10:33 +0100

    Added buttons and text box

--- app/src/main/java/com/kascayd/prototype/MainActivity.java
+++ app/src/main/java/com/kascayd/prototype/MainActivity.java
@@ -2,6 +2,8 @@ package com.kascayd.prototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartPress(View view) {
+        String message = "You have parked at _____";
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
